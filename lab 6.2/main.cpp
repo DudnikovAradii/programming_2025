7faa8c11b0d23ade0a4dcea5eb9e450bdf01edea
DudnikovAradii <arkobu301@gmail.com>
2025-11-21 16:29:26 +0200

    Update main.cpp

--- lab 6.2/main.cpp
+++ lab 6.2/main.cpp
@@ -2,37 +2,38 @@
 
 using namespace std;
 
-void matrixResult(int top, int left, int** mas) {
-    // Просто выводим матрицу
-    for (int i = 0; i < top; i++) {
-        for (int j = 0; j < left; j++) {
-            cout << mas[i][j] << " ";
+void matrixResult(int**& massiv, int& stroki, int stolbtsy) {
+    // Сначала выводим исходную матрицу
+    cout << "Исходная матрица:" << endl;
+    for (int i = 0; i < stroki; i++) {
+        for (int j = 0; j < stolbtsy; j++) {
+            cout << massiv[i][j] << " ";
         }
         cout << endl;
     }
-}
 
-void removeZeroRows(int**& mas, int& top, int left) {
-    int coZ = 0;
-    int* zeroRow = 0;
-    // Есть ли нули в строке или нет (если есть хоть 1 нолик, то переходим на следующую строку)
-    for (int i = 0; i < top; i++) {
-        for (int j = 0; j < left; j++) {
-            if (mas[i][j] == 0) {
-                coZ++;
+    // Затем обрабатываем строки с нулями
+    int schetchikNuley = 0;
+    int* strokiSNulyami = 0;
+
+    // Считаем строки с нулями
+    for (int i = 0; i < stroki; i++) {
+        for (int j = 0; j < stolbtsy; j++) {
+            if (massiv[i][j] == 0) {
+                schetchikNuley++;
                 break;
             }
         }
     }
 
-    // Выделяем память для coZ значений, туда мы поместим индексы строк, которые содержат нули.
-    if (coZ > 0) {
-        zeroRow = (int*)malloc(coZ * sizeof(int));
+    // Если есть строки с нулями
+    if (schetchikNuley > 0) {
+        strokiSNulyami = (int*)malloc(schetchikNuley * sizeof(int));
         int index = 0;
-        for (int i = 0; i < top; i++) {
-            for (int j = 0; j < left; j++) {
-                if (mas[i][j] == 0) {
-                    zeroRow[index] = i;
+        for (int i = 0; i < stroki; i++) {
+            for (int j = 0; j < stolbtsy; j++) {
+                if (massiv[i][j] == 0) {
+                    strokiSNulyami[index] = i;
                     index++;
                     break;
                 }
@@ -40,128 +41,133 @@ void removeZeroRows(int**& mas, int& top, int left) {
         }
 
         cout << "Строки с нулями: ";
-        for (int i = 0; i < coZ; i++) {
-            cout << zeroRow[i] << " ";
+        for (int i = 0; i < schetchikNuley; i++) {
+            cout << strokiSNulyami[i] << " ";
         }
         cout << endl;
 
-        // Удаляем строки с нулями из исходного массива
-        int newTop = top;
-        for (int i = 0; i < newTop; i++) {
-            bool flZ = false;
+        // Удаляем строки с нулями
+        int novyeStroki = stroki;
+        for (int i = 0; i < novyeStroki; i++) {
+            bool estNol = false;
 
             // Проверяем, есть ли в строке нули
-            for (int j = 0; j < left; j++) {
-                if (mas[i][j] == 0) {
-                    flZ = true;
+            for (int j = 0; j < stolbtsy; j++) {
+                if (massiv[i][j] == 0) {
+                    estNol = true;
                     break;
                 }
             }
 
             // Если нашли строку с нулем, удаляем её
-            if (flZ) {
-                free(mas[i]);
+            if (estNol) {
+                free(massiv[i]);
 
                 // Сдвигаем все последующие строки на одну позицию вверх
-                for (int k = i; k < newTop - 1; k++) {
-                    mas[k] = mas[k + 1];
+                for (int k = i; k < novyeStroki - 1; k++) {
+                    massiv[k] = massiv[k + 1];
                 }
-                newTop--;
+                novyeStroki--;
                 i--;
             }
         }
 
         // Изменяем размер массива указателей
-        if (newTop < top) {
-            mas = (int**)realloc(mas, newTop * sizeof(int*));
-            top = newTop;
+        if (novyeStroki < stroki) {
+            massiv = (int**)realloc(massiv, novyeStroki * sizeof(int*));
+            stroki = novyeStroki;
         }
 
         // Выводим результат после удаления строк с нулями
-        for (int i = 0; i < top; i++) {
-            for (int j = 0; j < left; j++) {
-                cout << mas[i][j] << " ";
+        cout << "Матрица после удаления строк с нулями:" << endl;
+        for (int i = 0; i < stroki; i++) {
+            for (int j = 0; j < stolbtsy; j++) {
+                cout << massiv[i][j] << " ";
             }
             cout << endl;
         }
+
+        free(strokiSNulyami);
+    }
+    else {
+        cout << "Строк с нулями не найдено" << endl;
     }
 }
 
 int main() {
+    setlocale(LC_ALL, "Ru");
     // создаём 2-х мерный массив с помощью 2-х указателей
-    int** mas = (int**)malloc(2 * sizeof(int*));
+    int** massiv = (int**)malloc(2 * sizeof(int*));
 
     for (int i = 0; i < 2; i++) {
-        mas[i] = (int*)malloc(2 * sizeof(int));
+        massiv[i] = (int*)malloc(2 * sizeof(int));
     }
 
+    // Заполняем матрицу с проверкой на отрицательные числа в первой строке
+    cout << "Введите 4 числа для матрицы 2x2:" << endl;
     for (int i = 0; i < 2; i++) {
+        cout << (i == 0 ? "Первая строка (A и B): " : "Вторая строка (C и D): ");
         for (int j = 0; j < 2; j++) {
-            cin >> mas[i][j];
-            if (i == 0 && mas[i][j] < 0) {
-                int fl = 1;
-                while (fl == 1) {
-                    cout << "Введите новое НЕ отрицательное число: ";
-                    cin >> mas[i][j];
-                    if (mas[i][j] >= 0) {
-                        fl = 0;
-                    }
-                }
+            while (true) {
+                cin >> massiv[i][j];
+                if (i != 0 || massiv[i][j] >= 0) break;
+                cout << "A и B должны быть неотрицательными. Повторите ввод: ";
             }
         }
     }
+    // Сохраняем исходные значения матрицы
+    int A = massiv[0][0];
+    int B = massiv[0][1];
+    int C = massiv[1][0];
+    int D = massiv[1][1];
 
-    int A = mas[0][0];
-    int B = mas[0][1];
-    int C = mas[1][0];
-    int D = mas[1][1];
+    // Вычисляем новые размеры матрицы
+    int stroki = 2 + A;
+    int stolbtsy = 2 + B;
 
-    int top = 2 + A;
-    int left = 2 + B;
+    // Расширяем массив с помощью функции realloc
+    massiv = (int**)realloc(massiv, stroki * sizeof(int*));
 
-    // расширяю массив с помощью функции realloc
-    mas = (int**)realloc(mas, top * sizeof(int*));
-
-    for (int i = 2; i < top; i++) {
-        mas[i] = (int*)malloc(left * sizeof(int));
+    // Выделяем память для новых строк
+    for (int i = 2; i < stroki; i++) {
+        massiv[i] = (int*)malloc(stolbtsy * sizeof(int));
     }
 
-    // сохраняю данные и заполняем матрицу
-    int old_block[2][2];
+    // Сохраняем исходный блок 2x2
+    int ishodnyyBlok[2][2];
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
-            old_block[i][j] = mas[i][j];
+            ishodnyyBlok[i][j] = massiv[i][j];
         }
     }
 
     // Расширяем существующие строки 0 и 1
     for (int i = 0; i < 2; i++) {
-        mas[i] = (int*)realloc(mas[i], left * sizeof(int));
+        massiv[i] = (int*)realloc(massiv[i], stolbtsy * sizeof(int));
     }
 
     // Заполняем ВСЮ матрицу формулой
-    for (int i = 0; i < top; i++) {
-        for (int j = 0; j < left; j++) {
-            mas[i][j] = i * C + j * D;
+    for (int i = 0; i < stroki; i++) {
+        for (int j = 0; j < stolbtsy; j++) {
+            massiv[i][j] = i * C + j * D;
         }
     }
 
     // Восстанавливаем исходный блок в правом нижнем углу
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
-            mas[i + A][j + B] = old_block[i][j];
+            massiv[i + A][j + B] = ishodnyyBlok[i][j];
         }
     }
 
-    matrixResult(top, left, mas);
-
-    removeZeroRows(mas, top, left);
+    // Используем объединённую функцию для обработки матрицы
+    matrixResult(massiv, stroki, stolbtsy);
 
     // Освобождаем память
-    for (int i = 0; i < top; i++) {
-        free(mas[i]);
+    for (int i = 0; i < stroki; i++) {
+        free(massiv[i]);
     }
-    free(mas);
+    free(massiv);
 
     return 0;
 }
